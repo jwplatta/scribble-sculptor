3bbf6cadaf212d290c0fc5c267104f11d16461c4
Joseph Platta <jwplatta@gmail.com>
2023-01-24 21:38:02 -0800

    remove the loop in the workflow of the instruct modal

--- InputInstructionModal.ts
+++ InputInstructionModal.ts
@@ -2,64 +2,7 @@ import { App, Modal, Editor, TextAreaComponent, ButtonComponent } from 'obsidian
 import { OpenAITextCompletion } from 'OpenAITextCompletion';
 
 
-class DraftCompletionModal extends Modal {
-  sourceText: string;
-  editor: Editor;
-  app: App;
-  textCompletion: OpenAITextCompletion;
-  draftCompletion: string;
-
-  constructor(app: App, editor: Editor, sourceText: string, draftCompletion: string, textCompletion: OpenAITextCompletion) {
-    super(app);
-    this.app = app;
-		this.editor = editor;
-		this.sourceText = sourceText;
-    this.draftCompletion = draftCompletion.trim();
-		this.textCompletion = textCompletion;
-  }
-
-  onOpen() {
-    const { contentEl } = this;
-
-    const textAreaDiv = contentEl.createDiv();
-    textAreaDiv.setAttr("style", "padding-top: 20px;");
-
-    const textArea = new TextAreaComponent(textAreaDiv);
-    textArea.setValue(this.draftCompletion);
-    textArea.inputEl.style.width = "100%";
-    textArea.inputEl.style.height = "20rem";
-    textArea.onChange((value) => {
-      this.draftCompletion = value;
-    })
-
-    const footerDiv = contentEl.createDiv();
-    footerDiv.setAttr("style", "padding-top: 5px;");
-
-    const useDraftButton = new ButtonComponent(footerDiv);
-    useDraftButton.setButtonText("Use");
-    useDraftButton.onClick(() => {
-      this.close();
-      const selectionWithDraft = this.sourceText + "\n\n" + this.draftCompletion.trim() + "\n";
-      this.editor.replaceSelection(selectionWithDraft);
-    });
-    useDraftButton.then((cb) => {
-      cb.buttonEl.setAttr("style", "margin: 2px;")
-    })
-
-    const tryAgainButton = new ButtonComponent(footerDiv);
-    tryAgainButton.setButtonText("Try again");
-    tryAgainButton.onClick(() => {
-      this.close()
-      new InputInstructionModal(this.app, this.editor, this.sourceText, this.textCompletion).open();
-    });
-    tryAgainButton.then((cb) => {
-      cb.buttonEl.setAttr("style", "margin: 2px;")
-    })
-
-  }
-}
-
-class ApplyingInstructionbModal extends Modal {
+class ApplyingInstructionModal extends Modal {
   sourceText: string;
   instructionText: string;
   editor: Editor;
@@ -86,7 +29,8 @@ class ApplyingInstructionbModal extends Modal {
     contentEl.createEl("p", { text: "Generating text completion..."})
     this.applyInstruction().then((draft) => {
       this.close();
-      new DraftCompletionModal(this.app, this.editor,this.sourceText, draft, this.textCompletion).open();
+      const selectionWithDraft = this.sourceText + "\n\n" + draft.trim() + "\n";
+      this.editor.replaceSelection(selectionWithDraft);
     })
   }
 
@@ -115,15 +59,11 @@ export class InputInstructionModal extends Modal {
 		const {contentEl} = this;
     contentEl.createEl("h3", { text: "Selected Text:" }).setAttribute("style", "margin-top: 20px;");
     contentEl.createEl("p", { text: this.sourceText });
-
-    contentEl.createEl("hr");
-
     contentEl.createEl("p", { text: "What do you want to do with the selected text?"})
 
     const textArea = new TextAreaComponent(contentEl);
     textArea.inputEl.style.width = "100%";
     textArea.inputEl.style.height = "10rem";
-
     textArea.onChange((value) => {
       this.instructionText = value;
     })
@@ -135,11 +75,8 @@ export class InputInstructionModal extends Modal {
     submitButton.setButtonText("Apply");
     submitButton.onClick(() => {
       this.close();
-      new ApplyingInstructionbModal(this.app, this.editor, this.sourceText, this.instructionText, this.textCompletion).open()
+      new ApplyingInstructionModal(this.app, this.editor, this.sourceText, this.instructionText, this.textCompletion).open()
     });
-    submitButton.then((cb) => {
-      cb.buttonEl.setAttr("style", "margin: 2px;");
-    })
 	}
 
 	onClose() {
